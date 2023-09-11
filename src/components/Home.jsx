8c8a6355c662cca84ac0a0ab6c7c1535c822548f
excitedcrayon <bonganimkonto@gmail.com>
2023-09-11 21:16:50 +1000

    started working on Homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,13 @@ const Home = () => {
     return (
         <>
             <div className="page-content">
-                <h1>Home</h1>
+                <div className="page-header">
+                    <h1>Welcome to Cablemakers</h1>
+                </div>
+                <article>
+                    <p>Are you looking for a wide range of cable types from leading manufacturers? Do you need a reliable supplier who will get you what you want on time and within budget?</p>
+                    <p>Cablemakers is the comprehensive cable solution you have been searching for! Supplying every market sector, including the mining, industrial, commercial, domestic and project sectors, Cablemakers has the ability to purchase in bulk from reliable cable manufacturers and distribute directly to your site, ensuring minimal down time and cost.</p>
+                </article>
             </div>
         </>
     );
