e2dcddf9f409727bcfbf22967d1779446b72af15
excitedcrayon <bonganimkonto@gmail.com>
2023-10-01 23:10:17 +1000

    added images for About in public folder and started working on About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,6 +9,22 @@ const About = () => {
                 <div className="page-header services-header" data-header-name="About">
                     <h1>About</h1>
                 </div>
+                <div className="articles">
+                    <article>
+                        <p>Cablemakers is an Australian independently owned bulk cable management and distribution business. We have strategically positioned distribution centres that provide reliable, comprehensive cabling solutions on time and on budget. We service every market including resources, industrial, commercial, government, power distribution and residential.</p>
+                        <p>Our professional and friendly staff have extensive cable experience and will source all your cable requirements at a market competitive price. We purchase in bulk from reliable, highly specialised cable manufacturers, which ensures we can get what you need quickly and at the right price. Access to a national branch network means you can rely on us to deliver the right solutions, first time, on time.</p>
+                        <p>We can cut-to-length and distribute directly to your site, ensuring minimal down time and cost. We also have heavy duty cable cutting machines which can handle cables of all weights and sizes. No matter how large or small your requirements we are equipped to service you. We have extensive experience in project management servicing multi- million dollar projects with staggered delivery dates and multiple locations.</p>
+                    </article>
+                    <article className="article-header-image sub-header-image">
+                        <h2>Why Do Business With Us?</h2>
+                    </article>
+                    <article className="article-header-image">
+                        <h2>Our Experience</h2>
+                        <p>We deliver cost-effective, complete cable solutions to some of the most demanding industries and significant projects throughout Australia such as mine sites, sugar mills, commercial buildings, industrial processing plants, electrical wholesalers, utilities and infrastructure projects. Major projects often require us to manage the cable supply chain process from the initial order placement to project completion, or to create innovative solutions which leverage our advanced business practices and world-class experience.</p>
+                        <p>We provide stock support and technical expertise which ensures our supply and service levels are consistent and not impacted by issues such as weather or manufacturing delays. We have extensive levels of inventory across multiple cable categories in stock at any given time so you won’t have any down time waiting for cable supplies.</p>
+                        <p>Cablemakers orders in bulk economic order quantities (EOQ) from reliable, specialised Australian and international cable manufacturers, to ensure highly competitive pricing and fast delivery. We can provide local cutting, drumming, labelling and documentation, which provides flexibility in terms of changing project schedules and unexpected requirements. This significantly lowers the risk associated with delivery delays, installation rescheduling costs, and labour/equipment downtime. Local co-operation with the site combined with integrated cable management ensures only the product required at any given point in time is on site. The lengths are cut from master drums using software to continuously optimise the cuts based on actual consumption and schedule status of the project. This reduces site congestion, lowers the risk of incorrect installation, and allows for more accurate site measurements, thereby reducing waste, time and money.</p>
+                    </article>
+                </div>
             </div>
         </motion.div>
     );
