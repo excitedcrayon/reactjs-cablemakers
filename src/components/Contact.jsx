f1cb51f1c604604cec20e34bcf8f1b2f6a9ac2fe
bgwdeveloper <bmkonto@bgwgroup.com.au>
2023-10-03 17:26:04 +1000

    started working on form validation for contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import branchesData from "../assets/file/branches.json";
 import { motion } from "framer-motion";
 import { pagevariant } from "../animation/PageVariant";
@@ -5,6 +6,46 @@ import { pagetransition } from "../animation/PageTransition";
 import BranchLocation from "./common/BranchLocation";
 
 const Contact = () => {
+
+    const [formData, setFormData] = useState({
+        name: '',
+        contact: '',
+        email: '',
+        enquiry: '',
+    });
+
+    const [error, setErrors] = useState({})
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData, [name] : value // map back to name, contact, email etc
+        });
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const validationErrors = {}
+        if(!formData.name.trim()){
+            validationErrors.name = "Name is required";
+        }
+        if(!formData.contact.trim()){
+            validationErrors.contact = "Contact number is required";
+        }
+        if(!formData.email.trim()){
+            validationErrors.email = "Email is required";
+        }
+        if(!formData.enquiry.trim()){
+            validationErrors.enquiry = "Your enquiry/comment is required";
+        }
+
+        setErrors(validationErrors);
+
+        if(Object.keys(validationErrors).length === 0){
+            console.log('Form submitted');
+        }
+    }
+
     return(
         <motion.div initial="out" animate="in" exit="out" variants={pagevariant} transition={pagetransition}>
             <div className="page-wrapper">
@@ -17,29 +58,33 @@ const Contact = () => {
                             <h2>Send Us A Message</h2>
                         </div>
                         <div className="contact-form">
-                            <form autoComplete="on">
+                            <form autoComplete="off" onSubmit={handleSubmit}>
                                 <div className="form-required">
                                     <small><sup>*</sup> Required fields</small>
                                 </div>
                                 <div className="form-row">
                                     <label>Name <sup>*</sup></label>
-                                    <input type="text" aria-label="Name Field" name="name"/>
+                                    <input type="text" aria-label="Name Field" name="name" onChange={handleChange}/>
+                                    {error.name && <span>{error.name}</span>}
                                 </div>
                                 <div className="form-row">
-                                    <label>Company Name</label>
-                                    <input type="text" aria-label="Company Name Field" name="company_name"/>
+                                    <label>Company</label>
+                                    <input type="text" aria-label="Company Name Field" name="company" onChange={handleChange}/>
                                 </div>
                                 <div className="form-row">
                                     <label>Contact Number <sup>*</sup></label>
-                                    <input type="text" aria-label="Contact Number Field" name="contact_number"/>
+                                    <input type="text" aria-label="Contact Number Field" name="contact" onChange={handleChange}/>
+                                    {error.contact && <span>{error.contact}</span>}
                                 </div>
                                 <div className="form-row">
                                     <label>Email <sup>*</sup></label>
-                                    <input type="text" aria-label="Email Field" name="contact_number"/>
+                                    <input type="text" aria-label="Email Field" name="email" onChange={handleChange}/>
+                                    {error.email && <span>{error.email}</span>}
                                 </div>
                                 <div className="form-row">
                                     <label>Enquiry/Comment <sup>*</sup></label>
-                                    <textarea name="enquiry_comment" aria-label="Enquriry/Comment Text Area" cols="30" rows="10"></textarea>
+                                    <textarea name="enquiry" aria-label="Enquriry/Comment Text Area" cols="30" rows="10" onChange={handleChange}></textarea>
+                                    {error.enquiry && <span>{error.enquiry}</span>}
                                 </div>
                                 <div className="form-submit">
                                     <button type="submit" title="Submit Form" aria-label="Submit button for the contact form">Submit</button>
