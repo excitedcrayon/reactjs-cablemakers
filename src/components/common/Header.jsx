cd4fa01f86d7b147dfc7082e3eaec539972274f2
excitedcrayon <bonganimkonto@gmail.com>
2023-10-16 22:39:47 +1000

    made Header responsive on mobile devices, fixed some CSS elements for header, footer and body

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,7 +1,21 @@
 import { NavLink} from "react-router-dom";
 import Breadcrumb from "../common/Breadcrumb";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+
+    const [activeMenuIcon, setActiveMenuIcon] = useState(false);
+    const toggleMenuIcon = () => {
+        setActiveMenuIcon(current => !current);
+    }
+
+    useEffect(() => {
+        if(activeMenuIcon){
+            document.body.classList.add('halt-body');
+        }else{
+            document.body.classList.remove('halt-body');
+        }
+    });
 
     return (
         <>
@@ -9,18 +23,25 @@ const Header = () => {
             <div className="header-content">
                 <div className="header-logo">
                     <div id="logo">
-                        <span className="logo-cable">Cable</span>
-                        <span className="logo-makers">Makers</span>
+                        <NavLink to='/' title="Cablemakers" aria-label="Cablemakers Logo for Homepage">
+                            <span className="logo-cable">Cable</span>
+                            <span className="logo-makers">Makers</span>
+                        </NavLink>
+                        <div className={activeMenuIcon ? "header-menu-icon active-menu-icon" : "header-menu-icon"} onClick={toggleMenuIcon}>
+                            <div className="menu-icon"></div>
+                            <div className="menu-icon"></div>
+                            <div className="menu-icon"></div>
+                        </div>
                     </div>
                 </div>
-                <nav>
+                <nav className={activeMenuIcon ? "active-nav-menu" : ""}>
                     <ul>
-                        <li className="header-nav-link"><NavLink to='/' className={({ isActive }) => (isActive ? 'active-link' : '')} title="Home" aria-label="Home Navigation Link">Home</NavLink></li>
-                        <li className="header-nav-link"><NavLink to='/products' className={({ isActive }) => (isActive ? 'active-link' : '')} title="Products" aria-label="Products Navigation Link">Products</NavLink></li>
-                        <li className="header-nav-link"><NavLink to='/services' className={({ isActive }) => (isActive ? 'active-link' : '')} title="Services" aria-label="Services Navigation Link">Services</NavLink></li>
-                        <li className="header-nav-link"><NavLink to='/projects' className={({ isActive }) => (isActive ? 'active-link' : '')} title="Projects" aria-label="Projects Navigation Link">Projects</NavLink></li>
-                        <li className="header-nav-link"><NavLink to='/about' className={({ isActive }) => (isActive ? 'active-link' : '')} title="About" aria-label="About Navigation Link">About</NavLink></li>
-                        <li className="header-nav-link"><NavLink to='/contact' className={({ isActive }) => (isActive ? 'active-link' : '')} title="Contact" aria-label="Contact Navigation Link">Contact</NavLink></li>
+                        <li className="header-nav-link"><NavLink onClick={() => setActiveMenuIcon(false)} to='/' className={({ isActive }) => (isActive ? 'active-link' : '')} title="Home" aria-label="Home Navigation Link">Home</NavLink></li>
+                        <li className="header-nav-link"><NavLink onClick={() => setActiveMenuIcon(false)} to='/products' className={({ isActive }) => (isActive ? 'active-link' : '')} title="Products" aria-label="Products Navigation Link">Products</NavLink></li>
+                        <li className="header-nav-link"><NavLink onClick={() => setActiveMenuIcon(false)} to='/services' className={({ isActive }) => (isActive ? 'active-link' : '')} title="Services" aria-label="Services Navigation Link">Services</NavLink></li>
+                        <li className="header-nav-link"><NavLink onClick={() => setActiveMenuIcon(false)} to='/projects' className={({ isActive }) => (isActive ? 'active-link' : '')} title="Projects" aria-label="Projects Navigation Link">Projects</NavLink></li>
+                        <li className="header-nav-link"><NavLink onClick={() => setActiveMenuIcon(false)} to='/about' className={({ isActive }) => (isActive ? 'active-link' : '')} title="About" aria-label="About Navigation Link">About</NavLink></li>
+                        <li className="header-nav-link"><NavLink onClick={() => setActiveMenuIcon(false)} to='/contact' className={({ isActive }) => (isActive ? 'active-link' : '')} title="Contact" aria-label="Contact Navigation Link">Contact</NavLink></li>
                     </ul>
                 </nav>
             </div>
