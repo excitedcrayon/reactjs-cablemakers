e19016f627751029ce888372ca6a995ce205f003
excitedcrayon <bonganimkonto@gmail.com>
2023-10-20 20:51:25 +1000

    added ScrollToTop functionality component to fix render not going back to the top of the page

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App.jsx';
+import ScrollToTop from "./components/ScrollToTop.jsx";
 import './index.css';
 import './css/constants.css';
 import './css/header.css';
@@ -11,6 +12,7 @@ import './css/footer.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
